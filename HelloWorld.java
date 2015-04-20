d807227e5573086bdb25b9c1d7a1dbdc88b00cdf
Ramnath Muthukumar <hi2ram@gmail.com>
2015-04-20 19:14:25 +0530

    Git is cool i am commiting a new file and a changed file

--- HelloWorld.java
+++ HelloWorld.java
@@ -3,6 +3,8 @@ public class HelloWorld
   public static void main(String args[])
   {
     //added comment
+    //added 2nd comment
+    //added 3rd comment
     System.print.outln("Hello World");
   }
 }
