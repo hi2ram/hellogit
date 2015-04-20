df9a387d08cc7f1d83db911e38d79b5af90d481f
Ramnath Muthukumar <hi2ram@gmail.com>
2015-04-20 19:22:43 +0530

    Changes done to print from a Method
    - add new method print()
    - create instance of HelloWorld
    - call print() method

--- HelloWorld.java
+++ HelloWorld.java
@@ -6,5 +6,15 @@ public class HelloWorld
     //added 2nd comment
     //added 3rd comment
     System.print.outln("Hello World");
+    //Create a new HelloWorld Instance
+    HelloWorld hello = new HelloWorld();
+    //Call the print method
+    hello.print();
   }
+
+  public void print()
+  {
+    System.print.outln("Hello World from Print()");
+  }
+
 }
